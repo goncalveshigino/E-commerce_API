83da3344772355cb7e101ed2c488c7b9be6bd8d4
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-04 08:33:28 +0100

    Adicionando a logica de login no userController

--- API/controllers/UserContoller.js
+++ API/controllers/UserContoller.js
@@ -43,4 +43,51 @@ class UserController {
             .then(() => res.json({ user: user.enviarAuthJSON() }))
             .catch(next);
     }
+
+
+    //PUT /
+    update(req, res, next) {
+        const { name, email, password } = req.body;
+        User.findById(req.payload.id).then((user) => {
+            if (!user) return res.status(401).json({ errors: "Usuairo nao registrado" });
+
+            if (typeof name !== undefined) user.name = name;
+            if (typeof email !== undefined) user.email = email;
+            if (typeof password !== undefined) user.setPassword(password);
+
+
+            return user.save().then(() => {
+                return res.json({ user: user.enviarAuthJSON() });
+            }).catch(next);
+        }).catch(next)
+    }
+
+    //DELETE /
+    remove(req, res, next) {
+        User.findById(req.payload.id).then(user => {
+            if (!user) return res.status(401).json({ errors: "Usuairo nao registrado" });
+
+            return user.remove().then(() => {
+                return res.json({ deletado: true });
+            }).catch(next);
+        }).catch(next);
+    }
+
+
+    //POST /LOGIN
+
+    login(req, res, next) {
+        const { email, password } = req.body;
+
+        if (!email) return res.status(422).json({ errors: { email: "nao pode ficar vazio" } });
+        if (!password) return res.status(422).json({ errors: { password: "nao pode ficar vazio" } });
+
+
+        User.findOne({ email }).then((user) => {
+            if (!user) return res.status(401).json({ errors: "Usuario nao registrado" });
+            if (!user.passwordValidator(password)) return res.status(401).json({ errors: "Senha Inválida" });
+
+            return res.json({ user: user.enviarAuthJSON() });
+        }).catch(next);
+    }
 }
